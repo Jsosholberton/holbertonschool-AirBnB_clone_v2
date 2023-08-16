929fa146c9d043605fdc18b37254abc74cbc054c
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-16 12:27:52 -0500

    filter added to advance task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -21,16 +21,37 @@ from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.place import Place
+from models.city import City
 
+dict_instances = {"State": State, "Amenity": Amenity,
+                  "Place": Place,"City": City}
 
 app = Flask(__name__)
 
-
+@app.route('/hbnb/<inst>/<id>', strict_slashes=False)
 @app.route('/hbnb', strict_slashes=False)
-def state_list(id=None):
+def state_list(inst=None, id=None):
+    places = storage.all(Place)
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    places = storage.all(Place)
+    cities = storage.all(City)
+
+    if inst == 'State' and id:
+        state = states["State." + id]
+        if state:
+            places = {key: place for key, place in places.items()\
+                      if place.cities.state_id == id}
+    elif inst == 'City' and id:
+        city = cities["City." + id]
+        if city:
+            places = {key: place for key, place in places.items()\
+                      if place.city_id == id}
+    elif inst == "Amenity":
+        amty = amenities["Amenity." + id]
+        if amty:
+            places = {key: place for key, place in places.items()\
+                      if any(amenity.id == id for amenity in place.amenities)}
+
     return render_template('100-hbnb.html',
                            states=states,
                            amenities=amenities,
